bee73cade9cc59228b3e19a1c2e5b5f657be95cd
talafarael <127351345+talafarael@users.noreply.github.com>
2024-08-28 21:39:11 +0300

    f

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
+const  {createFolder,createFile}=require("./utils/createFileAndFolder");
+const pathSwitch=require("./utils/path")
 const fs = require("fs")
-const number=10;
-const jumpFol=4;
-function rec(num,jump,path){
+let sum=0
+const number=5;
+const jumpFol=6;
+async function rec(num,jump,path){
    
     // let num=3;
-    const dir=`${path}/dir${num}`
+    const dir=pathSwitch(path,num);
     if(num==0){
         return ;
     }
-    if(jump==0){
+    if(jump==-1){
         console.log("end")
         return 0;
     }
-    if (!fs.existsSync(dir)){
-        fs.mkdirSync(dir, { recursive: true });
-    }  
-    if(num%2==0){
-     fs.writeFileSync(`${dir}.txt`, 'hell', 'utf8');
-    }
+    const math=Math.floor(Math.random()*2)
+    if(jump!=-1){
+    createFolder(dir)
+}
+    console.log(math)
+    if(math==1&&jump!=jumpFol){
 
-      console.log("jump"+jump)
+        createFile(dir)
+    }
+     
     rec(number,jump=jump-1,dir)
-  
     num-=1
     rec(num,jump+1,path)
 
 }
-rec(number,jumpFol,".")
+function readFile(num,jump,path){
+     dir=pathSwitch(path,num);
+    if(num==0){
+        return ;
+    }
+    if(jump==-1){
+        console.log("end")
+        return 0;
+    }
+    fs.readdirSync(dir).forEach(file => {
+        if(file.split('.')[1]){
+            sum++
+            
+        }
+      });
+     
+      readFile(number,jump=jump-1,dir);
+  
+    num-=1
+    readFile(num,jump+1,path)
+}
+rec(number,jumpFol,"./dir")
+
+readFile(number,jumpFol,"./dir")
+console.log(sum)
--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
-//  void rec(int n){
-//     rec(n--);
-//     std::cout<<n;
-//      rec(n++);
-//  std::cout<<n;
-// }
-// main(){
+#include <iostream>
+#include <filesystem>
+#include <string>
+
+ const int number=4;
+const int jumpFol=10;
+ 
+void rec(int num,int jump,std::string path){
+namespace fs = std::filesystem;
+std::string dir = path + "/dir"+std::to_string(num) ;
+ if(num==0){
+        return ;
+    }
+    if(jump==-1){
+        
+        return ;
+    }
+if(jump!=-1){
+    fs::create_directories(dir);
+}
+int math;
+  std::string dir="./";
+ if(math==1&&jump!=jumpFol){
+
+        //create file
+    }
+    
+  rec(number,jump=jump-1,dir);
+    num--;
+    rec(num,jump+1,path);
+}
+int main(){
+    std::cout<<"Aaa";
 //  rec(5);
-// }
+return ;
+}
