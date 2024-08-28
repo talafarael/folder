403c038f49ea0ad329ab173e94fac2f854321342
talafarael <127351345+talafarael@users.noreply.github.com>
2024-08-28 22:22:14 +0300

    radme and end recursive

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 const  {createFolder,createFile}=require("./utils/createFileAndFolder");
 const pathSwitch=require("./utils/path")
 const fs = require("fs")
+const endRecursion=require("./utils/endRecursion")
 let sum=0
-const number=5;
-const jumpFol=6;
+const number=2;
+const jumpFol=2;
 async function rec(num,jump,path){
    
     // let num=3;
     const dir=pathSwitch(path,num);
-    if(num==0){
-        return ;
-    }
-    if(jump==-1){
-        console.log("end")
-        return 0;
-    }
+   const end=endRecursion(num,jump)
+   if(end){
+    return;
+   }
     const math=Math.floor(Math.random()*2)
     if(jump!=-1){
     createFolder(dir)
 }
     console.log(math)
-    if(math==1&&jump!=jumpFol){
+    if(math==1 && jump!=jumpFol){
 
         createFile(dir)
     }
@@ -32,13 +30,10 @@ async function rec(num,jump,path){
 }
 function readFile(num,jump,path){
      dir=pathSwitch(path,num);
-    if(num==0){
-        return ;
-    }
-    if(jump==-1){
-        console.log("end")
-        return 0;
-    }
+     const end=endRecursion(num,jump)
+     if(end){
+      return;
+     }
     fs.readdirSync(dir).forEach(file => {
         if(file.split('.')[1]){
             sum++
---
+++ utils/endRecursion.js
@@ -0,0 +1,10 @@
+function endRecursion(num,jump) {
+    if(num==0){
+        return true;
+    }
+    if(jump==-1){
+        console.log("end")
+        return true;
+    }
+  }
+  module.exports=endRecursion;
